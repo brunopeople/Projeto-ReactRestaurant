053bfc510a380bda7583fe8d053d59b3e51696f7
bruno pessoa nunes de melo <brunopessoa.melo@gmail.com>
2022-02-18 10:36:55 -0300

    Codificando o componente Menuitem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+// eslint-disable-next-line no-unused-vars
 import {AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu} from './container';
 import {Navbar} from './components';
 
